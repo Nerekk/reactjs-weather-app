5b41989e2885a5eafeea79988945655b929923a6
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-05-26 19:54:57 +0200

    Search improvements

--- src/pages/weather/weather-body/locations/CitySearch.jsx
+++ src/pages/weather/weather-body/locations/CitySearch.jsx
@@ -3,82 +3,116 @@ import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import CircularProgress from "@mui/material/CircularProgress";
 import Axios from "axios";
-import { useQuery } from "@tanstack/react-query";
+import {useQuery} from "@tanstack/react-query";
+import {Button} from "@mui/material";
+import {useContext} from "react";
+import {LocationsContext} from "./Locations.jsx";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 const API_KEY = "a79aad9743859b7143100ca7247efd7c";
 
 const fetchOptions = async (query) => {
-  const response = await Axios.get(
-    `https://api.openweathermap.org/geo/1.0/direct?q=${query}&appid=${API_KEY}&limit=5`
-  );
-  return response.data.map(
-    (item) => `${item.name}, ${item.country}, ${item.state}`
-  );
+    const response = await Axios.get(
+        `https://api.openweathermap.org/geo/1.0/direct?q=${query.replace(' ', '+')}&appid=${API_KEY}&limit=5`
+    );
+    return response.data.map(
+        (item) => ({
+            name: `${item.name}`,
+            description: `${item.country}, ${item.state}`,
+            lat: item.lat,
+            lon: item.lon,
+        })
+    );
 };
 
 export const CitySearch = () => {
-  const [open, setOpen] = React.useState(false);
-  const [inputValue, setInputValue] = React.useState("");
+    const [open, setOpen] = React.useState(false);
+    const [inputValue, setInputValue] = React.useState("");
+    const [selectedOption, setSelectedOption] = React.useState(null);
 
-  const {
-    data: options,
-    isLoading,
-    refetch,
-  } = useQuery({
-    queryKey: ["fetchOptions", inputValue],
-    queryFn: () => fetchOptions(inputValue),
-    enabled: false,
-  });
+    const {locations, setLocations} = useContext(LocationsContext);
 
-  React.useEffect(() => {
-    if (inputValue) {
-      refetch();
+    const {
+        data: options,
+        isLoading,
+        refetch,
+    } = useQuery({
+        queryKey: ["fetchOptions", inputValue],
+        queryFn: () => fetchOptions(inputValue),
+        enabled: false,
+    });
+
+    React.useEffect(() => {
+        if (inputValue) {
+            refetch();
+        }
+    }, [inputValue, refetch]);
+
+    const isUnique = (selected) => {
+        const check = locations.find((location) => location.name === selected.name);
+
+        return check == null;
     }
-  }, [inputValue, refetch]);
 
-  React.useEffect(() => {
-    if (!open) {
-      setInputValue("");
-    }
-  }, [open]);
+    const addLocation = () => {
+        if (selectedOption && isUnique(selectedOption)) {
+            setLocations((prevLocations) => [...prevLocations, selectedOption]);
+            console.log("LOCATIONS: ", [...locations, selectedOption]);
+        }
+    };
 
-  return (
-    <div>
-      <Autocomplete
-        id="asynchronous-demo"
-        sx={{ width: 300 }}
-        open={open}
-        onOpen={() => {
-          setOpen(true);
-        }}
-        onClose={() => {
-          setOpen(false);
-        }}
-        inputValue={inputValue}
-        onInputChange={(event, newInputValue) => {
-          setInputValue(newInputValue);
-        }}
-        options={options || []}
-        loading={isLoading}
-        isOptionEqualToValue={(option, value) => option === value}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Search city"
-            InputProps={{
-              ...params.InputProps,
-              endAdornment: (
-                <React.Fragment>
-                  {isLoading ? (
-                    <CircularProgress color="inherit" size={20} />
-                  ) : null}
-                  {params.InputProps.endAdornment}
-                </React.Fragment>
-              ),
-            }}
-          />
-        )}
-      />
-    </div>
-  );
+    const opts = options && options.map((op) => `${op.name}, ${op.description}`);
+
+    return (
+        <>
+        <div>
+            <Typography sx={{mt: 4, mb: 2}} variant="h6" component="div">
+                Locations list
+            </Typography>
+        </div>
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+            <Autocomplete
+                id="asynchronous-demo"
+                sx={{width: 300}}
+                open={open}
+                onOpen={() => {
+                    setOpen(true);
+                }}
+                onClose={() => {
+                    setOpen(false);
+                }}
+                inputValue={inputValue}
+                onInputChange={(event, newInputValue) => {
+                    setInputValue(newInputValue);
+                }}
+                onChange={(event, newValue) => {
+                    setSelectedOption(
+                        options.find((option) => `${option.name}, ${option.description}` === newValue)
+                    );
+                }}
+                options={options ? opts : []}
+                loading={isLoading}
+                isOptionEqualToValue={(option, value) => option === value}
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        label="Search city"
+                        InputProps={{
+                            ...params.InputProps,
+                            endAdornment: (
+                                <React.Fragment>
+                                    {isLoading ? (
+                                        <CircularProgress color="inherit" size={20}/>
+                                    ) : null}
+                                    {params.InputProps.endAdornment}
+                                </React.Fragment>
+                            ),
+                        }}
+                    />
+                )}
+            />
+            <Button onClick={addLocation}>Add to list</Button>
+        </Box></>
+    );
 };
