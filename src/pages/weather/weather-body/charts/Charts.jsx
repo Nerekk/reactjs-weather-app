a3ae5b7026e8f53e2cb6d40fae3d1b015b7f4b39
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-05-26 21:58:27 +0200

    Addition of formating functions to displaying charts info

--- src/pages/weather/weather-body/charts/Charts.jsx
+++ src/pages/weather/weather-body/charts/Charts.jsx
@@ -1,10 +1,11 @@
 import {styled} from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
-import {ChartTemp} from "./ChartTemp";
 import {ChartHum} from "./ChartHum";
 import {ChartPressure} from "./ChartPressure";
 import {ChartWind} from "./ChartWind";
+import {ChartTemp} from "./ChartTemp.jsx";
+import Typography from "@mui/material/Typography";
 
 const Item = styled(Paper)(({theme}) => ({
     backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -14,13 +15,30 @@ const Item = styled(Paper)(({theme}) => ({
     color: theme.palette.text.secondary,
 }));
 
+export const getWeekDayChart = (date) => {
+    const dayOfWeek = date.toLocaleDateString("en-US", {weekday: "long"});
+
+    const today = new Date();
+
+    const isToday =
+        date.getFullYear() === today.getFullYear() &&
+        date.getMonth() === today.getMonth() &&
+        date.getDay() === today.getDay();
+    return isToday ? "Today" : dayOfWeek;
+};
+
+export const getHourChart = (date) => {
+    return `${date.getHours()}:00`;
+};
+
 export const Charts = () => {
     return (
         <Grid container rowSpacing={1} columnSpacing={{xs: 1, sm: 2, md: 3}}>
-            <Grid item xs={6}>
-                <Item><ChartTemp/></Item>
+            <Grid item xs={8}>
+                <Typography>Temperature</Typography>
+                <Item><ChartTemp /></Item>
             </Grid>
-            <Grid item xs={6}>
+            <Grid item xs={4}>
                 <Item><ChartHum/></Item>
             </Grid>
             <Grid item xs={6}>
