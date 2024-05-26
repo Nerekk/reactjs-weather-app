2139e12cf1b7e2338925882efe4bef18205b8e86
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-05-26 12:47:15 +0200

    Implementation of place for incoming charts

--- src/pages/weather/weather-body/WeatherBody.jsx
+++ src/pages/weather/weather-body/WeatherBody.jsx
@@ -6,6 +6,7 @@ import { MainWeather } from "./MainWeather";
 import { ForecastWeather } from "./ForecastWeather";
 import { Button } from "@mui/material";
 import { useState } from "react";
+import { ChartsBody } from "./charts/ChartsBody";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -44,7 +45,9 @@ export const WeatherLayout = () => {
             </Item>
           </Grid>
           <Grid item xs={8}>
-            <Item>xs=8</Item>
+            <Item>
+              <ChartsBody />
+            </Item>
           </Grid>
           <Grid item xs={4}>
             <Item>xs=4</Item>
