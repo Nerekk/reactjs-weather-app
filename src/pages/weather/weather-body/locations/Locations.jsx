3638a3167df21b3c6b0fdb9790575ff040373782
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-05-26 19:55:38 +0200

    Locations list implementation to UI and useContext for location list management

--- src/pages/weather/weather-body/locations/Locations.jsx
+++ src/pages/weather/weather-body/locations/Locations.jsx
@@ -1,10 +1,18 @@
 import { Box } from "@mui/material";
 import { CitySearch } from "./CitySearch";
+import {CityList} from "./CityList.jsx";
+import {createContext, useState} from "react";
+
+export const LocationsContext = createContext();
 
 export const Locations = () => {
+    const [locations, setLocations] = useState([]);
     return (
         <Box>
-            <CitySearch/>
+            <LocationsContext.Provider value={{locations, setLocations}}>
+                <CitySearch/>
+                <CityList/>
+            </LocationsContext.Provider>
         </Box>
     );
 }
