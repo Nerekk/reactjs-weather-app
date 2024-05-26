08c17ccb16fc967919ce938d19bc67c57c3b0b69
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-05-26 22:00:51 +0200

    Temperature info chart implementation

--- src/pages/weather/weather-body/charts/ChartTemp.jsx
+++ src/pages/weather/weather-body/charts/ChartTemp.jsx
@@ -1,18 +1,57 @@
-import {Box} from "@mui/material";
-import {LineChart} from "@mui/x-charts";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { ResponsiveChartContainer } from '@mui/x-charts/ResponsiveChartContainer';
+import { LinePlot } from '@mui/x-charts/LineChart';
+
+import { ChartsXAxis } from '@mui/x-charts/ChartsXAxis';
+import { ChartsYAxis } from '@mui/x-charts/ChartsYAxis';
+import {useContext, useEffect, useState} from "react";
+import {DataChartsContext} from "../WeatherBody.jsx";
+import {getHourChart, getWeekDayChart} from "./Charts.jsx";
 
 export const ChartTemp = () => {
+    const {dataF, isloadingF} = useContext(DataChartsContext);
+    const [timeData, setTimeData] = useState([]);
+    const [tempData, setTempData] = useState([]);
+
+    useEffect(() => {
+        if (dataF) {
+            const temp = dataF.list.map((d) => d.main.temp);
+            const time = dataF.list.map((d) => new Date(d.dt * 1000));
+            setTempData(temp);
+            setTimeData(time);
+        }
+    }, [dataF]);
+
+
+    if (isloadingF) {
+        return <h1>Loading..</h1>;
+    }
+
+    const config = {
+        series: [
+            { type: 'line', data: tempData },
+        ],
+        height: 300,
+        xAxis: [
+            {
+                data: timeData,
+                scaleType: 'time',
+                valueFormatter: (date) =>
+                    date.getHours() === 0
+                        ? getWeekDayChart(date)
+                        : getHourChart(date),
+            },
+        ],
+    };
+
     return (
-        <Box sx={{width: "100%"}}>
-            <LineChart
-                xAxis={[{data: [1, 2, 3, 5, 8, 10]}]}
-                series={[
-                    {
-                        data: [2, 5.5, 2, 8.5, 1.5, 5],
-                    },
-                ]}
-                height={300}
-            />
+        <Box sx={{ width: '100%' }}>
+            <ResponsiveChartContainer {...config}>
+                <LinePlot />
+                <ChartsXAxis />
+                <ChartsYAxis />
+            </ResponsiveChartContainer>
         </Box>
     );
-};
+}
