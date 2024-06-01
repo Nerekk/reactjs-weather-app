671d8d255f47f80796003aa8b269eded15c96e7b
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-06-01 21:19:37 +0200

    Addition of appbar version for logged user

--- src/pages/weather/weather-appbar/WeatherAppbar.jsx
+++ src/pages/weather/weather-appbar/WeatherAppbar.jsx
@@ -3,27 +3,61 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import {useNavigate} from "react-router-dom";
+import Grid from "@mui/material/Grid";
+import useAuth from "../../../auth/useAuth.js";
+
+const logout = (auth, setAuth) => {
+    // zniszcz token poprzez backend
+
+    setAuth(null);
+}
 
 export const WeatherAppBar = () => {
+    const navigate = useNavigate();
+    const {auth, setAuth} = useAuth();
+
     return (
         <Box sx={{flexGrow: 1}}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{mr: 2}}
-                    >
-                        <MenuIcon/>
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-                        Weather
-                    </Typography>
-                    <Button color="inherit">Login</Button>
+                    <Grid container alignItems="center">
+                        <Grid item xs={4}>
+                            <Typography variant="h6" component="div">
+                                Weather
+                            </Typography>
+                        </Grid>
+
+                        {!auth ?
+
+                            (<><Grid item xs={4}>
+                                <Typography align="center">
+                                    You are not logged in!
+                                </Typography>
+                            </Grid>
+                                <Grid item xs={4} sx={{textAlign: 'right'}}>
+                                    <Button color="inherit" onClick={() => navigate('/')}>Login</Button>
+                                </Grid></>)
+                            :
+                            (<><Grid item xs={4}>
+                                <Typography align="center">
+                                    Welcome {auth.username}!
+                                </Typography>
+                            </Grid>
+                                <Grid item xs={4} sx={{textAlign: 'right'}}>
+                                    <Button color="inherit" onClick={() => {
+                                        navigate('/weather');
+                                    }}>Weather</Button>
+                                    <Button color="inherit" onClick={() => {
+                                        navigate('/profile');
+                                    }}>Profile</Button>
+                                    <Button color="inherit" onClick={() => {
+                                        logout(auth, setAuth);
+                                        navigate('/');
+                                    }}>Logout</Button>
+                                </Grid></>)
+                        }
+                    </Grid>
                 </Toolbar>
             </AppBar>
         </Box>
