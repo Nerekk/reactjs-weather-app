ff2e203b9444aeca168c00badb08a489d3747f4f
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-06-01 21:23:05 +0200

    Redirected api calls to backend which is handling weather data

--- src/pages/weather/weather-body/WeatherBody.jsx
+++ src/pages/weather/weather-body/WeatherBody.jsx
@@ -9,6 +9,7 @@ import {Charts} from "./charts/Charts";
 import {Locations} from "./locations/Locations";
 import {useQuery} from "@tanstack/react-query";
 import Axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 const Item = styled(Paper)(({theme}) => ({
     backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -19,11 +20,7 @@ const Item = styled(Paper)(({theme}) => ({
 }));
 
 
-const URL_C = "https://api.openweathermap.org/data/2.5/weather";
-const URL_F = "https://api.openweathermap.org/data/2.5/forecast";
-const URL_AP = "https://api.openweathermap.org/data/2.5/air_pollution";
-const API_KEY = "a79aad9743859b7143100ca7247efd7c";
-const UNITS = "metric";
+const URL_BACKEND = "http://localhost:8080/api/weather/combined/";
 
 const isValid = (arg, targetHour) => {
     const dt = arg;
@@ -34,23 +31,42 @@ const isValid = (arg, targetHour) => {
     return hour === targetHour;
 };
 
-const getQueryURL = (url, city) => {
-    return `${url}?appid=${API_KEY}&units=${UNITS}&q=${city.name},${city.country}`;
-};
 
-const getQueryURLForPollution = (url, lat, lon) => {
-    return `${url}?appid=${API_KEY}&lat=${lat}&lon=${lon}`;
-};
+const getQueryBackend = (city) => {
+    return `${URL_BACKEND}${city.name},${city.country}`;
+}
 
-const getData = async (queryUrl) => {
-    const data = await Axios.get(queryUrl).then((res) => res.data);
-    // if (filtered) {
-    //     const filteredData = data.list.filter((d) => isValid(d.dt, 15));
-    //     return filteredData;
-    // } else {
-    //     return data;
-    // }
-    return data;
+const getData = async (queryUrl, navigate) => {
+    try {
+        const response = await Axios.get(
+            queryUrl,
+            // {
+            //     headers:
+            //         {
+            //             'Authorization': `Bearer ${token}`
+            //         }
+            // },
+            {
+                withCredentials: true
+            }
+            );
+        console.log('Status Code:', response.status);
+        console.log('Data:', response.data);
+        return response.data;
+    } catch (error) {
+        if (error.response) {
+            console.error('Error Status Code:', error.response.status);
+            console.error('Error Data:', error.response.data);
+            navigate('/error', { state: { status: error.response.status, message: error.response.data } });
+        } else if (error.request) {
+            console.error('No Response:', error.request);
+            navigate('/error', { state: { status: 500, message: error.message } });
+        } else {
+            console.error('Error', error.message);
+            navigate('/error', { state: { status: 500, message: error.message } });
+        }
+        throw error;
+    }
 };
 
 export const GlobalContext = createContext();
@@ -67,32 +83,45 @@ export const WeatherLayout = () => {
         lon: 21.0067249,
     });
     const [filteredData, setFilteredData] = useState([]);
+    const [dataC, setDataC] = useState(null);
+    const [dataF, setDataF] = useState(null);
+    const [dataAP, setDataAP] = useState(null);
 
-    const {data: dataC, isLoading: isLoadingC, refetch: refetchC} = useQuery({
-        queryKey: ["current"],
-        queryFn: () => getData(getQueryURL(URL_C, city)),
-    });
+    const [isDataLoaded, setIsDataLoaded] = useState(false);
 
-    const {data: dataF, isLoading: isLoadingF, refetch: refetchF} = useQuery({
-        queryKey: ["forecast"],
-        queryFn: () => getData(getQueryURL(URL_F, city)),
-    });
+    const navigate = useNavigate();
 
-    const {data: dataAP, isLoading: isLoadingAP, refetch: refetchAP} = useQuery({
-        queryKey: ["air_pollution"],
-        queryFn: () => getData(getQueryURLForPollution(URL_AP, city.lat, city.lon)),
+    const {data, isLoading, refetch, error} = useQuery({
+        queryKey: ["combined"],
+        queryFn: () => getData(getQueryBackend(city), navigate),
     });
 
 
     useEffect(() => {
-        refetchC();
-        refetchF();
-        refetchAP();
-        if (dataF && dataF.list) {
-            setFilteredData(dataF.list.filter((d) => isValid(d.dt, 15)));
+        if (data) {
+            setDataC(data.weather);
+            setDataF(data.forecast);
+            setDataAP(data.air);
+            if (data.forecast && data.forecast.list) {
+                setFilteredData(data.forecast.list.filter((d) => isValid(d.dt, 15)));
+            }
         }
-        console.log("done");
-    }, [city, dataF, refetchAP, refetchC, refetchF]);
+        setIsDataLoaded(true);
+        console.log("done: " + data);
+    }, [data]);
+
+    useEffect(() => {
+        refetch();
+    }, [city, refetch]);
+
+    if (isLoading || !isDataLoaded) {
+        return <h1>Loading..</h1>;
+    }
+
+    if (error) {
+        console.log("ERORREK: " + error);
+        navigate('/error');
+    }
 
     return (
         <>
@@ -101,21 +130,21 @@ export const WeatherLayout = () => {
                     <Grid container spacing={2}>
                         <Grid item xs={4}>
                             <Item>
-                                <DataCurrentContext.Provider value={{dataC, isLoadingC, refetchC, dataAP, isLoadingAP}}>
+                                <DataCurrentContext.Provider value={{dataC, isLoading, dataAP}}>
                                     <MainWeather/>
                                 </DataCurrentContext.Provider>
                             </Item>
                         </Grid>
                         <Grid item xs={8}>
                             <Item>
-                                <DataForecastContext.Provider value={{filteredData, isLoadingF, refetchF}}>
+                                <DataForecastContext.Provider value={{filteredData, isLoading}}>
                                     <ForecastWeather/>
                                 </DataForecastContext.Provider>
                             </Item>
                         </Grid>
                         <Grid item xs={9}>
                             <Item>
-                                <DataChartsContext.Provider value={{dataF, isLoadingF}}>
+                                <DataChartsContext.Provider value={{dataF, isLoading}}>
                                     <Charts/>
                                 </DataChartsContext.Provider>
                             </Item>
