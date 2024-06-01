9063554ee852d2cd2622a6e658089defff2fbde8
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-06-01 21:17:56 +0200

    Use of ReqiureAuth in app.jsx and implementation of new routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import {WeatherPage} from "./pages/weather/WeatherPage";
 import {LoginPage} from "./pages/login/LoginPage";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {ErrorPage} from "./pages/error/ErrorPage.jsx";
+import RequireAuth from "./auth/RequireAuth.jsx";
+import {ProfilePage} from "./pages/profile/ProfilePage.jsx";
 
 function App() {
     const client = new QueryClient({
@@ -19,7 +22,11 @@ function App() {
                     <QueryClientProvider client={client}>
                         <Routes>
                             <Route path={'/'} element={<LoginPage/>}/>
-                            <Route path={'/weather'} element={<WeatherPage/>}/>
+                            <Route element={<RequireAuth />}>
+                                <Route path={'/weather'} element={<WeatherPage/>}/>
+                                <Route path={'/profile'} element={<ProfilePage/>}/>
+                            </Route>
+                            <Route path={"/error"} element={<ErrorPage />} />
                         </Routes>
                     </QueryClientProvider>
                 </BrowserRouter>
