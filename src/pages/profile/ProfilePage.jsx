c0179dbf83e41f6e5feff313a7ecb35c8e905b2a
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-06-01 21:17:35 +0200

    Profile page basic creation

---
+++ src/pages/profile/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import {WeatherAppBar} from "../weather/weather-appbar/WeatherAppbar.jsx";
+
+
+export const ProfilePage = () => {
+
+    return (
+        <div>
+            <WeatherAppBar/>
+        Profile page
+    </div>);
+}
