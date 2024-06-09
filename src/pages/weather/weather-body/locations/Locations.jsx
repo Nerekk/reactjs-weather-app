a49542db674f10b6f6f4ed1c2fe270e466a74b5f
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-06-09 21:14:47 +0200

    Get rid of locations context

--- src/pages/weather/weather-body/locations/Locations.jsx
+++ src/pages/weather/weather-body/locations/Locations.jsx
@@ -1,18 +1,13 @@
 import {Box} from "@mui/material";
 import {CitySearch} from "./CitySearch";
 import {CityList} from "./CityList.jsx";
-import {createContext, useState} from "react";
-
-export const LocationsContext = createContext();
 
 export const Locations = () => {
-    const [locations, setLocations] = useState([]);
+
     return (
         <Box>
-            <LocationsContext.Provider value={{locations, setLocations}}>
-                <CitySearch/>
-                <CityList/>
-            </LocationsContext.Provider>
+            <CitySearch/>
+            <CityList/>
         </Box>
     );
 }
